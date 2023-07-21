5e2127d5e9cda18a0c1975382606de92a6d15f67
RITIK_KUMAR_KESHRIi <71667776+rkcoder093@users.noreply.github.com>
2023-07-21 21:14:55 +0530

    update 0.3

--- artuniverse/settings.py
+++ artuniverse/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d#6m8b+vdx9_k7m@vd7+cpy-tdx(#=m9liw#_!$=)!46(#7r#1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -118,9 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'website/static'),
-]
+STATICFILES_DIRS =os.path.join(BASE_DIR, 'website/static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
